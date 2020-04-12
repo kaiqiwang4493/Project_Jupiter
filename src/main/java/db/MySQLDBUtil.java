5bae0bd1e18f3337c1a3ac0030c361277c7d8f42
Kaiqi Wang <kaiqiwang4493@gmail.com>
2020-04-11 20:33:53 -0700

    safty

--- src/main/java/db/MySQLDBUtil.java
+++ src/main/java/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package db;
 
 public class MySQLDBUtil {
-	private static final String INSTANCE = "laiproject-instance.c6q7x0cbyjt4.us-west-1.rds.amazonaws.com";
+	private static final String INSTANCE = "find it in local file";
 	private static final String PORT_NUM = "3306";
 	public static final String DB_NAME = "laiproject";
-	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "wkq900312";
+	private static final String USERNAME = "find it in local file";
+	private static final String PASSWORD = "find it in local file";
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
